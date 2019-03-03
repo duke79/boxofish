a8b9624b03c3c802f094a36de5b62870750245bb
duke79 <pulkitsingh01@gmail.com>
2019-03-03 20:36:59 +0530

    cordova likes HashRouter better

--- client/src/routes/App.js
+++ client/src/routes/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Redirect, Route, /*Link*/} from "react-router-dom";
+import {HashRouter as Router, Redirect, Route, /*Link*/} from "react-router-dom";
 
 import MovieList from "./MovieList";
 import Menu from "../components/Menu";
