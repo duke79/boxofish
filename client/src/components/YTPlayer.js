5065bdffef4e49255af255441b339a4802103fd0
duke79 <pulkitsingh01@gmail.com>
2019-03-09 15:22:25 +0530

    trying to pause YouTube video on cordova "pause" (but perhaps it's having no effect! cordova seems to take care of pausing itself)

--- client/src/components/YTPlayer.js
+++ client/src/components/YTPlayer.js
@@ -21,17 +21,60 @@ const YoutubeWrapper = styled.div`
 `;
 
 class YTPlayer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            players: []
+        };
+        this.onReady = this.onReady.bind(this);
+        this.pause = this.pause.bind(this);
+    }
+
+    onReady(e) {
+        const players = this.state.players;
+        players.push(e.target);
+        this.setState({
+            players: players
+        });
+
+        if (this.props.onReady)
+            this.props.onReady();
+    }
+
+    pause() {
+        this.state.players.forEach((player) => {
+            player.pauseVideo();
+        });
+    }
+
+    componentDidMount() {
+        // Add the deviceready event
+        document.addEventListener("deviceready", function(){
+
+            // attach events
+            document.addEventListener("pause", function () {
+                console.log("pausingg!");
+                this.pause();
+            }.bind(this), false);
+        }.bind(this), false);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener("pause", this.pause, false);
+    }
+
     render() {
         return (
             <YoutubeWrapper {...this.props}>
-                <YouTube {...this.props} />
+                <YouTube {...this.props} onReady={this.onReady}/>
             </YoutubeWrapper>
         );
     }
 }
 
 YTPlayer.defaultProps = {
-    videoId: "2g811Eo7K8U"
+    videoId: "2g811Eo7K8U",
+    host: "https://www.youtube.com"
 };
 
 export default YTPlayer;
